0656ba1dc0646cfe239b14e478d986e1e426c82b
ransell <ransell@bu.edu>
2015-02-01 00:31:59 -0500

    Idealist Shop, Button to/from Formal

---
+++ Assets/src/Floor1UI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor1UI : MonoBehaviour {
+
+	TabMenuController controller;
+
+	public void Start() {
+		controller = TabMenuController.GetInstance();
+	}              
+
+	public void LoadFormal() {
+		controller.Forward("test");
+	}
+}
