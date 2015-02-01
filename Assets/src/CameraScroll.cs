8e1e3440910e7bc3c6eebe6cd744c690b43d80ad
ransell <ransell@bu.edu>
2015-01-31 23:46:16 -0500

    Escalator UP and scroll buttons

---
+++ Assets/src/CameraScroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScroll : MonoBehaviour {
+
+	public Camera camera;
+
+	// Update is called once per frame
+	void Update () {
+		float xAxisValue = Input.GetAxis("Horizontal");
+		if (Camera.current != null) {
+			camera.transform.Translate(new Vector3(xAxisValue, 0.0f, 0.0f));
+		}
+	}
+}
