42f3a3b5d6c4d99c4a10fd14b4ebfcc3f53554a9
Leo Denault <leo.denault@gmail.com>
2015-02-06 12:19:58 -0500

    Committing release of current work for hosting on Heroku

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express();
-app.use('/static', express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '/Release'));
 
 app.get("/", function (req, res) {
 	console.log(req.url);
     res.writeHead(200);
-    res.write(fs.readFileSync(__dirname + "/index.html"));
+    res.write(fs.readFileSync(__dirname + "/Release/Release.html"));
     res.end();
 });
 
