c17eb3ae5ad04a04552a9807ca472cba0e681900
ransell <ransell@bu.edu>
2015-02-14 21:40:44 -0500

    Toggling Equip/Unequip Button in Wardrobe

--- Assets/src/Wardrobe.cs
+++ Assets/src/Wardrobe.cs
@@ -3,11 +3,28 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Wardrobe : MonoBehaviour {
+    private bool isEquipped;
 
     public Button equipButton;
+    public Sprite equipImage;
     public Sprite unequipImage;
 
-    public void Equip() {
-        equipButton.image.sprite = unequipImage;
+    public void Start()
+    {
+        isEquipped = false;
+    }
+
+    public void Equip(){
+        isEquipped = !isEquipped;
+
+        if (isEquipped)
+        {
+            equipButton.image.sprite = unequipImage;
+        }
+        else
+        {
+            equipButton.image.sprite = equipImage;
+        }
+        
     }
 }
