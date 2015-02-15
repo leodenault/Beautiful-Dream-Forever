58571e97a89d270e539c12aed97c61bc3b9f2868
Leo Denault <leo.denault@gmail.com>
2015-02-14 20:47:50 -0500

    Added logic for changing equip button background

---
+++ Assets/src/Wardrobe.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Wardrobe : MonoBehaviour {
+
+    public Button equipButton;
+    public Sprite unequipImage;
+
+    public void Equip() {
+        equipButton.image.sprite = unequipImage;
+    }
+}
