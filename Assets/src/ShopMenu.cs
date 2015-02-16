50bb70b62bc641b439b002b05eef05b59f85c0da
ransell <ransell@bu.edu>
2015-02-15 21:55:43 -0500

    Shop Menu toggles

---
+++ Assets/src/ShopMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShopMenu : MonoBehaviour {
+
+    public GameObject showMenu;
+
+    public void ShowMenu()
+    {
+        showMenu.SetActive(!showMenu.activeSelf);
+        Debug.Log("Creeper Creeper Creeper");
+    }
+
+
+}
