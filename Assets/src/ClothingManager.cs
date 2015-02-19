edc3c9305ac5ddf0abdbf5a96b52a2eed9e8d9a4
Leo Denault <leo.denault@gmail.com>
2015-02-18 21:27:08 -0500

    Ensured that loaded files are readable on Heroku

--- Assets/src/ClothingManager.cs
+++ Assets/src/ClothingManager.cs
@@ -1,27 +1,34 @@
-﻿using System.IO;
+﻿using UnityEngine;
+using System.IO;
 using System.Xml.Serialization;
 
 public class ClothingManager {
-    private string file;
-    private ClothingData[] clothingData;
+	private string file;
+	private ClothingData[] clothingData;
 
-    public ClothingManager(string file) {
-        this.file = file;
-        load();
-    }
+	public ClothingManager(string file) {
+		this.file = file;
+		load();
+	}
 
 	private void load() {
 		XmlSerializer serializer = new XmlSerializer(typeof(ClothingData[]));
-		FileStream input = new FileStream(file, FileMode.Open);
-		clothingData = serializer.Deserialize(input) as ClothingData[];
-		input.Close();
+		TextAsset data = Resources.Load<TextAsset>(file);
+		byte[] bytes = data.bytes;
+
+		MemoryStream stream = new MemoryStream();
+		stream.Write(bytes, 0, bytes.Length);
+		stream.Seek(0, SeekOrigin.Begin);
+
+		clothingData = serializer.Deserialize(stream) as ClothingData[];
+		stream.Close();
 	}
 
-    public string[] GetClothingImages() {
-        string[] imageLocations = new string[clothingData.Length];
-        for (int i = 0; i < clothingData.Length; i++) {
-            imageLocations[i] = clothingData[i].Path;
-        }
-        return imageLocations;
-    }
+	public string[] GetClothingImages() {
+		string[] imageLocations = new string[clothingData.Length];
+		for (int i = 0; i < clothingData.Length; i++) {
+			imageLocations[i] = clothingData[i].Path;
+		}
+		return imageLocations;
+	}
 }
--- Assets/src/WardrobeController.cs
+++ Assets/src/WardrobeController.cs
@@ -4,50 +4,50 @@ using System;
 using System.Collections;
 
 public class WardrobeController {
-    private static WardrobeController INSTANCE;
-    private static float ITEM_WIDTH = 42.0f;
-    private static float ITEM_HEIGHT = 35.0f;
-    private static float SCALE_MODIFIER = 0.85f;
+	private static WardrobeController INSTANCE;
+	private static float ITEM_WIDTH = 42.0f;
+	private static float ITEM_HEIGHT = 35.0f;
+	private static float SCALE_MODIFIER = 0.85f;
 
-    private ClothingManager manager;
+	private ClothingManager manager;
 
-    private WardrobeController() {
-        this.manager = new ClothingManager("Assets/Resources/data/clothing.xml");
-    }
+	private WardrobeController() {
+		this.manager = new ClothingManager("data/clothing");
+	}
 
-    public static WardrobeController GetInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new WardrobeController();
-        }
+	public static WardrobeController GetInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new WardrobeController();
+		}
 
-        return INSTANCE;
-    }
+		return INSTANCE;
+	}
 
-    private float computeScale(float width1, float width2, float height1, float height2) {
-        float wScale = width1 / width2;
-        float hScale = height1 / height2;
+	private float computeScale(float width1, float width2, float height1, float height2) {
+		float wScale = width1 / width2;
+		float hScale = height1 / height2;
 
-        return Math.Min(wScale, hScale) * SCALE_MODIFIER;
-    }
+		return Math.Min(wScale, hScale) * SCALE_MODIFIER;
+	}
 
-    public void AssignClothingBackrounds(Button[] wardrobeButtons) {
-        string[] images = manager.GetClothingImages();
+	public void AssignClothingBackrounds(Button[] wardrobeButtons) {
+		string[] images = manager.GetClothingImages();
 
-        for (int i = 0; i < wardrobeButtons.Length && i < images.Length; i++) {
-            Image[] buttonImages = wardrobeButtons[i].GetComponentsInChildren<Image>();
+		for (int i = 0; i < wardrobeButtons.Length && i < images.Length; i++) {
+			Image[] buttonImages = wardrobeButtons[i].GetComponentsInChildren<Image>();
 
-            if (buttonImages.Length > 1) {
-                Image image = buttonImages[1];
-                Sprite sprite = Resources.Load<Sprite>(images[i]);
-                image.sprite = sprite;
+			if (buttonImages.Length > 1) {
+				Image image = buttonImages[1];
+				Sprite sprite = Resources.Load<Sprite>(images[i]);
+				image.sprite = sprite;
 
-                float spriteWidth = sprite.rect.width;
-                float spriteHeight = sprite.rect.height;
-                float scale = computeScale(ITEM_WIDTH, spriteWidth, ITEM_HEIGHT, spriteHeight);
+				float spriteWidth = sprite.rect.width;
+				float spriteHeight = sprite.rect.height;
+				float scale = computeScale(ITEM_WIDTH, spriteWidth, ITEM_HEIGHT, spriteHeight);
 
-                image.rectTransform.sizeDelta = new Vector2(spriteWidth * scale, spriteHeight * scale);
-                image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            }
-        }
-    }
+				image.rectTransform.sizeDelta = new Vector2(spriteWidth * scale, spriteHeight * scale);
+				image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+			}
+		}
+	}
 }
