9f3c1a2a16d94ad307b3510e6afd28bf64317cda
Leo Denault <leo.denault@gmail.com>
2015-02-20 15:47:01 -0500

    Updated data items for clothing

--- Assets/src/ClothingData.cs
+++ Assets/src/ClothingData.cs
@@ -2,6 +2,28 @@
 using System.Collections;
 
 public class ClothingData {
+	public enum ClothingSlot {
+		HAD, TOP, BOTTOM, DRESS, FEET, ACCESSORY
+	}
+
+	public enum ClothingStyle {
+		IDEALIST, HARDCORE, ATHLETIC, UNIFORM, FORMAL, COSPLAY, PREPPY, HIPSTER
+	}
+
+	public enum ClothingEssence {
+		CUTE, COOL, CLASSY
+	}
+
+	public enum ClothingTexture {
+		PVC, METAL, DENIM, COTTON, PATTERN, LEATHER
+	}
+	
 	public string Name;
 	public string Path;
+	public string Description;
+	public Vector2 Location;
+	public ClothingSlot Slot;
+	public ClothingStyle Style;
+	public ClothingEssence Essence;
+	public ClothingTexture Texture;
 }
