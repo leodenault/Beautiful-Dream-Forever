551a1127d357ede7ee14bf92b14d7dc4b5698b87
Leo Denault <leo.denault@gmail.com>
2015-02-20 20:27:33 -0500

    Removed debug line from shop menu

--- Assets/src/ui/ShopMenu.cs
+++ Assets/src/ui/ShopMenu.cs
@@ -9,7 +9,6 @@ public class ShopMenu : MonoBehaviour {
     public void ShowMenu()
     {
         showMenu.SetActive(!showMenu.activeSelf);
-        Debug.Log("Creeper Creeper Creeper");
     }
 
 
