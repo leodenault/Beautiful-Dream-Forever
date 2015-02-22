2924785b40ed0a1d013ed4befcdcd74b26c71298
ransell <ransell@bu.edu>
2015-02-22 02:07:14 -0500

    Athletic Shop Dressingroom

---
+++ Assets/src/ui/ShopkeeperButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopkeeperButton : MonoBehaviour
+{
+
+    GlobalController controller;
+
+    public void Start() {
+        controller = GlobalController.GetInstance();
+    }
+
+    public void LoadAthleticDressing() {
+        controller.Forward("Athletic Shop Dressingroom");
+    }
+
+}
