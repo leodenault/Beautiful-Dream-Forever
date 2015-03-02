2538e646c85be10c4c149f9356876cca9b002776
Leo Denault <leo.denault@gmail.com>
2015-03-02 18:48:18 -0500

    Set up battle screen to use clothing area and clothing slot system
    Currently there are white overlays over the slots in the background.
    Once these are correctly positioned, we should make the colour
    transparent

---
+++ Assets/src/ui/BattleScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+class BattleScreen : MonoBehaviour {
+	private ClothingArea clothingArea;
+	private ClothingSlotSystem clothingSlotSystem;
+	
+	public GameObject clothingAreaContainer;
+	public GameObject itemSlotsPanel;
+
+	public void Start() {
+		clothingArea = clothingAreaContainer.GetComponentInChildren<ClothingArea>();
+		clothingSlotSystem = itemSlotsPanel.GetComponentInChildren<ClothingSlotSystem>();
+		clothingSlotSystem.Init(clothingArea, null);
+	}
+}
