ce93cb60a75dc278ef313ae346c0dca50cacef78
Leo Denault <leo.denault@gmail.com>
2015-03-05 21:41:50 -0500

    Added onlick to battle button to go straight to battle screen

---
+++ Assets/src/ui/DressingRoom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+class DressingRoom : MonoBehaviour {
+	private GlobalController controller;
+
+	public void Start() {
+		controller = GlobalController.GetInstance();
+	}
+
+	public void LoadBattle() {
+		controller.Forward("Battle Screen");
+	}
+}
