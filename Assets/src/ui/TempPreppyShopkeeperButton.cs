b69226d473d97ea7a4adb0cbb8a2ba31d74d391f
ransell <ransell@bu.edu>
2015-03-19 01:40:28 -0400

    Added Preppy Dressingroom

---
+++ Assets/src/ui/TempPreppyShopkeeperButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempPreppyShopkeeperButton : MonoBehaviour
+{
+
+    GlobalController controller;
+
+    public void Start()
+    {
+        controller = GlobalController.GetInstance();
+    }
+
+    public void LoadPreppyDressing()
+    {
+        controller.Forward("Preppy Shop Dressingroom");
+    }
+
+}
