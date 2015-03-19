abe934c69484b29140bc5233e2b9cc8d8d93472f
Matthew Szczepankiewicz <mjszczep@buffalo.edu>
2015-03-19 00:11:34 -0400

    Added bounds on camera scroll.

--- Assets/src/ui/CameraScroll.cs
+++ Assets/src/ui/CameraScroll.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class CameraScroll : MonoBehaviour {
 
 	public Camera mallCamera;
+	private const float X_MIN = -1.8f;
+	private const float X_MAX = 1.8f;
 
 	// Update is called once per frame
 	void Update () {
 		float xAxisValue = Input.GetAxis("Horizontal");
 		if (Camera.current != null) {
+			float xCur = mallCamera.transform.position.x;
+			xAxisValue = Mathf.Clamp(xAxisValue, X_MIN - xCur, X_MAX - xCur);
 			mallCamera.transform.Translate(new Vector3(xAxisValue, 0.0f, 0.0f));
 		}
 	}
