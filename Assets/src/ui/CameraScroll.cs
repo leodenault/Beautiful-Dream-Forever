b92ee707e4c6fde9972395e4fe533b6c0c48e839
Matthew Szczepankiewicz <mjszczep@buffalo.edu>
2015-03-20 15:24:50 -0400

    Fixed camera scroll bounds (FOR REAL THIS TIME)

--- Assets/src/ui/CameraScroll.cs
+++ Assets/src/ui/CameraScroll.cs
@@ -2,12 +2,24 @@
 using System.Collections;
 
 public class CameraScroll : MonoBehaviour {
-
+	
 	public Camera mallCamera;
-
+	private float X_MIN;
+	private float X_MAX;
+	
+	void Start () {
+		/* Determine camera bounds based on screen size and stuff. Based on
+		 * http://answers.unity3d.com/questions/501893/calculating-2d-camera-bounds.html */
+		float cameraWidth = Camera.main.camera.orthographicSize * Screen.width / Screen.height;
+		Debug.Log (cameraWidth);
+		X_MIN = cameraWidth - 6.65f; //6.65 was determined by testing until it worked
+		X_MAX = 6.65f - cameraWidth;
+	}
+			
 	// Update is called once per frame
 	void Update () {
-		float xAxisValue = Input.GetAxis("Horizontal");
+		float curX = mallCamera.transform.position.x;
+		float xAxisValue = Mathf.Clamp(Input.GetAxis("Horizontal"), X_MIN - curX, X_MAX - curX);
 		if (Camera.current != null) {
 			mallCamera.transform.Translate(new Vector3(xAxisValue, 0.0f, 0.0f));
 		}
