483c0985544b508c8b95bfbaed405e9d06dd4baf
ransell <ransell@bu.edu>
2015-03-21 20:06:15 -0400

    Can "walk through" Floor 3
    Go to the different shops. The shops themselves are not set up yet.

---
+++ Assets/src/ui/Floor3UI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor3UI : MonoBehaviour
+{
+
+	GlobalController controller;
+
+	public void Start()
+	{
+		controller = GlobalController.GetInstance();
+	}
+
+	public void LoadIdealist()
+	{
+		controller.Forward("Idealist Shop");
+	}
+
+	public void LoadHardcore()
+	{
+		controller.Forward("Hardcore Shop");
+	}
+
+	public void LoadFloor4()
+	{
+		controller.Forward("Floor 4");
+	}
+
+	public void LoadFloor2()
+	{
+		controller.Forward("Floor 2");
+	}
+}
+
