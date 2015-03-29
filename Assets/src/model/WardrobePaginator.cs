50389e515cbf0d7942539f9bd5c8682e9da6c2ff
Leo Denault <leo.denault@gmail.com>
2015-03-29 00:54:39 -0400

    Ported WardrobePaginator from JS code into C#, with tests

---
+++ Assets/src/model/WardrobePaginator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class WardrobePaginator<T> {
+	private const int DEFAULT_PAGE_SIZE = 15;
+
+	private int pageSize;
+	private int index;
+	private IList<T[]> sets;
+
+	public WardrobePaginator(T[] data) {
+		init(data, DEFAULT_PAGE_SIZE);
+	}
+
+	public WardrobePaginator(T[] data, int pageSize) {
+		init(data, pageSize);
+	}
+	
+	private void init(T[] data, int pageSize) {
+		this.pageSize = pageSize;
+
+		sets = new List<T[]>();
+		index = 0;
+
+		for (int i = 0; i < data.Length; i += pageSize) {
+			int setSize = Math.Min(pageSize, data.Length - i);
+			T[] currentSet = new T[setSize];
+			Array.Copy(data, i, currentSet, 0, setSize);
+			sets.Add(currentSet);
+		}
+	}
+
+
+	public T[] Next() {
+		index = (index == sets.Count - 1) ? index : index + 1;
+		return sets[index];
+	}
+
+	public T[] Previous() {
+		index = (index > 0) ? index - 1 : index;
+		return sets[index];
+	}
+
+	public T[] Current() {
+		return sets[index];
+	}
+}
---
+++ Assets/test/Editor/WardrobePaginatorTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class WardrobePaginatorTest {
+
+	private int[] createData(int size) {
+		int[] data = new int[size];
+		for (int i = 1; i <= size; i++) {
+			data[i - 1] = i;
+		}
+		return data;
+	}
+
+	[Test]
+	public void PaginatorReturnsSamePageIfAtEndOfData() {
+		WardrobePaginator<int> paginator = new WardrobePaginator<int>(createData(16));
+		int[] page1 = paginator.Current();
+		int[] page2 = paginator.Next();
+		int[] page3 = paginator.Next();
+		Assert.AreNotEqual(page1, page2);
+		Assert.AreEqual(page2, page3);
+	}
+
+	[Test]
+	public void PaginatorHas15AsDefaultPageSize() {
+		WardrobePaginator<int> paginator15 = new WardrobePaginator<int>(createData(15));
+		WardrobePaginator<int> paginator16 = new WardrobePaginator<int>(createData(16));
+
+		Assert.AreEqual(15, paginator15.Current().Length);
+		Assert.AreEqual(15, paginator16.Current().Length);
+	}
+
+	[Test]
+	public void PreviousReturnsSamePageIfAtBeginningOfData() {
+		WardrobePaginator<int> paginator = new WardrobePaginator<int>(createData(16));
+		paginator.Next();
+		int[] page = paginator.Previous();
+		Assert.AreEqual(page, paginator.Previous());
+	}
+
+	[Test]
+	public void CurrentReturnsCurrentPage() {
+		WardrobePaginator<int> paginator = new WardrobePaginator<int>(createData(33));
+		Assert.AreEqual(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, paginator.Current());
+
+		int[] page = paginator.Next();
+		Assert.AreEqual(page, paginator.Current());
+
+		page = paginator.Next();
+		Assert.AreEqual(page, paginator.Current());
+	}
+
+	[Test]
+	public void NextReturnsNextSetOfData() {
+		WardrobePaginator<int> paginator = new WardrobePaginator<int>(createData(10), 3);
+		Assert.AreEqual(new int [] {1, 2, 3}, paginator.Current());
+		Assert.AreEqual(new int [] {4, 5, 6}, paginator.Next());
+		Assert.AreEqual(new int [] {7, 8, 9}, paginator.Next());
+		Assert.AreEqual(new int[] { 10 }, paginator.Next());
+	}
+
+	[Test]
+	public void PreviousReturnsPreviousSetOfData() {
+		WardrobePaginator<int> paginator = new WardrobePaginator<int>(createData(10), 3);
+		paginator.Next();
+		paginator.Next();
+		Assert.AreEqual(new int [] {10}, paginator.Next());
+		Assert.AreEqual(new int [] {7, 8, 9}, paginator.Previous());
+		Assert.AreEqual(new int [] {4, 5, 6}, paginator.Previous());
+		Assert.AreEqual(new int[] { 1, 2, 3 }, paginator.Previous());
+	}
+}
