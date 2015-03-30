442d6d9c1eb2801e3b523edad44dd610521d5798
Leo Denault <leo.denault@gmail.com>
2015-03-29 22:54:09 -0400

    Added automerge option to deployment script

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ from decimal import getcontext, Decimal
 SOURCE_REPO = "git@github.com:leodenault/fashion.git"
 STAGING_REPO = "git@heroku.com:fashion-game-staging.git"
 PROD_REPO = "git@heroku.com:fashion-game.git"
-USAGE_TEXT = "Usage: deploy.py [-h|--help] <environment>\n\nParameters:\n\tenvironment:\tEither \"staging\" for the staging environment or \"prod\" for the production environment"
+USAGE_TEXT = "Usage: deploy.py [-h|--help] [-m|--automerge] <environment>\n\nParameters:\n\tenvironment:\tEither \"staging\" for the staging environment or \"prod\" for the production environment"
 
 def execute_command(command, *args):
 	command_list = [command]
@@ -32,9 +32,10 @@ def get_git_branch():
 def parse_args():
 	opts = []
 	args = []
+	autoMerge = False
 	
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+		opts, args = getopt.getopt(sys.argv[1:], "hm", ["help", "automerge"])
 	except getopt.GetoptError:
 		print(USAGE_TEXT)
 		sys.exit(2)
@@ -43,63 +44,75 @@ def parse_args():
 		if opt in ("-h", "--help"):
 			print(USAGE_TEXT)
 			sys.exit()
+		if opt in ("-m", "--automerge"):
+			autoMerge = True
 
 	if len(args) != 1:
 		print(USAGE_TEXT)
 		sys.exit(2)
 	
 	if args[0] == "staging":
-		return True
+		return (True, autoMerge)
 	elif args[0] == "prod":
-		return False
+		return (False, autoMerge)
 	else:
 		print(USAGE_TEXT)
 		sys.exit(2)
 	
 # Parse the arguments given to the file
-isStaging = parse_args()
-
-# Get the previous tag number from Git
-tag_info = execute_command("git", "describe")
-dash_index = tag_info.find("-")
-previous_tag = tag_info[1:dash_index]
-print(str.format("Retrieved previous tag {tag} from Git", tag=tag_info[0:dash_index]))
-
-# Get major and minor versions
-decimal_index = previous_tag.find(".")
-major = previous_tag[:decimal_index]
-minor = previous_tag[decimal_index+1:]
-
-# Create the new tag number
-if isStaging:
-	minor = int(minor) + 1
-	next_tag = str.format("v{major}.{minor}", major=major, minor=minor)
-else:
-	major = int(major) + 1
-	next_tag = str.format("v{major}.{minor}", major=major, minor=0)
-
-
-#Get the tag message from the user
-tag_message = ""
-
-while tag_message == "":
-	tag_message = raw_input("Please enter a tag message: ")
+isStaging, autoMerge = parse_args()
 
 branch = get_git_branch();
 print(str.format("Detecting that Git is currently on {branch} branch", branch=branch))
 
-# Create the tag in Git
-execute_command("git", "tag", "-a", next_tag, "-m", tag_message)
-print(str.format("Created new tag {tag}", tag=next_tag))
+if (isStaging and branch == "staging") or (not isStaging and branch == "prod"):
+	# Get the previous tag number from Git
+	tag_info = execute_command("git", "describe")
+	dash_index = tag_info.find("-")
+	previous_tag = tag_info[1:dash_index]
+	print(str.format("Retrieved previous tag {tag} from Git", tag=tag_info[0:dash_index]))
 
-# Push the tag to Github
-env = isStaging ? "staging" : "prod"
-execute_command("git", "push", SOURCE_REPO, str.format("{branch}:{env}", branch=branch, env=env), "--tags")
-print(str.format("Pushed tag {tag} to {repo}", tag=next_tag, repo=SOURCE_REPO))
+	# Get major and minor versions
+	decimal_index = previous_tag.find(".")
+	major = previous_tag[:decimal_index]
+	minor = previous_tag[decimal_index+1:]
 
-# Push the tag and commit to Heroku staging
-repo = isStaging ? STAGING_REPO : PROD_REPO
-print(str.format("Pushing to {env} server {repo}", env=env, repo=repo))
-execute_command("git", "push", repo, str.format("{branch}:master", branch=branch), "--tags")
-print(str.format("Pushed tag {tag} to {repo}", tag=next_tag, repo=repo))
+	# Create the new tag number
+	if isStaging:
+		minor = int(minor) + 1
+		next_tag = str.format("v{major}.{minor}", major=major, minor=minor)
+	else:
+		major = int(major) + 1
+		next_tag = str.format("v{major}.{minor}", major=major, minor=0)
 
+
+	#Get the tag message from the user
+	tag_message = ""
+
+	while tag_message == "":
+		tag_message = raw_input("Please enter a tag message: ")
+
+	if autoMerge:
+		sourceBranch = "master" if isStaging else "staging"
+		destBranch = "staging" if isStaging else "prod"
+		print(str.format("Merging branch {src} into branch {dest}", src=sourceBranch, dest=destBranch))
+		execute_command("git", "merge", sourceBranch)
+	
+	# Create the tag in Git
+	execute_command("git", "tag", "-a", next_tag, "-m", tag_message)
+	print(str.format("Created new tag {tag}", tag=next_tag))
+
+	# Push the tag to Github
+	env = "staging" if isStaging else "prod"
+	execute_command("git", "push", SOURCE_REPO, str.format("{branch}:{env}", branch=branch, env=env), "--tags")
+	print(str.format("Pushed tag {tag} to {repo}", tag=next_tag, repo=SOURCE_REPO))
+
+	# Push the tag and commit to Heroku staging
+	repo = STAGING_REPO if isStaging else PROD_REPO
+	print(str.format("Pushing to {env} server {repo}", env=env, repo=repo))
+	execute_command("git", "push", repo, str.format("{branch}:master", branch=branch), "--tags")
+	print(str.format("Pushed tag {tag} to {repo}", tag=next_tag, repo=repo))
+
+else:
+	print(str.format("Please move to {branch} branch before proceeding", branch=("staging" if isStaging else "prod")))
+	sys.exit(2)
